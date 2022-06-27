4d39e99ccf2427090568241b201b0c235b4aff34
Mario Schmoker <103613451+MarioSchmoker@users.noreply.github.com>
2022-06-27 04:04:49 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+let dimensionArreglo: number = Number(
+  prompt("Ingrese la cantidad de celdas deseadas")
+);
+let indice: number;
+let arrayAños: number[] = new Array(dimensionArreglo);
+let arrayAltura: number[] = new Array(dimensionArreglo);
+let arrayNombres: string[] = new Array(dimensionArreglo);
+let edad: number;
+let altura: number;
+let nombre: string;
+let numPosicion: number;
+let i: number;
+let j: number;
+let k: number;
+
+for (numPosicion = 0; numPosicion < dimensionArreglo; numPosicion++) {
+  nombre = String(prompt(" Nombre " + (numPosicion + 1)));
+  edad = Number(prompt(" Edad " + (numPosicion + 1)));
+  altura = Number(prompt(" Altura " + (numPosicion + 1)));
+  i = 0;
+  while (i < numPosicion && arrayAños[i] < edad) {
+    i++;
+  }
+  for (j = numPosicion; j > i; j--) {
+    arrayNombres[j] = arrayNombres[j - 1];
+    arrayAños[j] = arrayAños[j - 1];
+    arrayAltura[j] = arrayAltura[j - 1];
+  }
+  arrayNombres[i] = nombre;
+  arrayAños[i] = edad;
+  arrayAltura[i] = altura;
+}
+
+for (indice = 0; indice < dimensionArreglo; indice++) {
+  console.log("El año en la posición ", indice + 1, " es ", arrayAños[indice]);
+  console.log(
+    "La altura ingresada en la posición ",
+    indice + 1,
+    " es ",
+    arrayAltura[indice]
+  );
+  console.log(
+    "El nombre ingresado en la posición ",
+    indice + 1,
+    " es ",
+    arrayNombres[indice]
+  );
+}
